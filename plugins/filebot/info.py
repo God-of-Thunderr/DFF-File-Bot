d3c5ffa3569c6a6f2f323acb946bbf1d5fb92259
God-of-Thunderr <99614535+God-of-Thunderr@users.noreply.github.com>
2022-06-06 13:27:51 +0530

    Update info.py

--- plugins/filebot/info.py
+++ plugins/filebot/info.py
@@ -1,1 +1,27 @@
+import os
+from .plugins.filebot. import tginfo
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+@Client.on_message((filters.private | filters.group) & filters.command(["info", "information"]))
+async def info(bot, update):
+    if (not update.reply_to_message) and ((not update.forward_from) or (not update.forward_from_chat)):
+        info = tginfo.user(update.from_user)
+    elif update.reply_to_message and update.reply_to_message.forward_from:
+        info = tginfo.user(update.reply_to_message.forward_from)
+    elif update.reply_to_message and update.reply_to_message.forward_from_chat:
+        info = tginfo.chat(update.reply_to_message.forward_from_chat)
+    elif (update.reply_to_message and update.reply_to_message.from_user) and (not update.forward_from or not update.forward_from_chat):
+        info = tginfo.user(update.reply_to_message.from_user)
+    else:
+        return
+    try:
+        await update.reply_text(
+            text=info+"\n\nMade by @subhan011",
+            reply_markup=BUTTONS,
+            disable_web_page_preview=True,
+            quote=True
+        )
+    except Exception as error:
+        await update.reply_text(error)
 
