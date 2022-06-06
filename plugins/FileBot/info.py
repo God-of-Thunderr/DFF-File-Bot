4d6545f0e4874d7450a76287e0a1c96d7c6f7635
God-of-Thunderr <99614535+God-of-Thunderr@users.noreply.github.com>
2022-06-06 13:11:01 +0530

    Create info.py

---
+++ plugins/FileBot/info.py
@@ -0,0 +1,56 @@
+import os
+import telegraminfo
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+Bot = Client(
+    "Info-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+START_TEXT = """Hello {},
+I am a user or chat information finder telegram bot.
+- Send /info for your info
+- Send /info reply to a forward message for chat or user info
+Made by @FayasNoushad"""
+
+BUTTONS = InlineKeyboardMarkup(
+    [[InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')]]
+)
+
+
+@Bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    reply_markup = BUTTONS
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=reply_markup,
+        quote=True
+    )
+
+
+@Bot.on_message((filters.private | filters.group) & filters.command(["info", "information"]))
+async def info(bot, update):
+    if (not update.reply_to_message) and ((not update.forward_from) or (not update.forward_from_chat)):
+        info = telegraminfo.user(update.from_user)
+    elif update.reply_to_message and update.reply_to_message.forward_from:
+        info = telegraminfo.user(update.reply_to_message.forward_from)
+    elif update.reply_to_message and update.reply_to_message.forward_from_chat:
+        info = telegraminfo.chat(update.reply_to_message.forward_from_chat)
+    elif (update.reply_to_message and update.reply_to_message.from_user) and (not update.forward_from or not update.forward_from_chat):
+        info = telegraminfo.user(update.reply_to_message.from_user)
+    else:
+        return
+    try:
+        await update.reply_text(
+            text=info+"\n\nMade by @FayasNoushad",
+            reply_markup=BUTTONS,
+            disable_web_page_preview=True,
+            quote=True
+        )
+    except Exception as error:
+        await update.reply_text(error)
