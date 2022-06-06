0c9b54b8048eabb13027ea3f171a0467bbcd3ea9
God-of-Thunderr <99614535+God-of-Thunderr@users.noreply.github.com>
2022-06-06 12:07:21 +0530

    Update NightMode.py

--- plugins/FileBot/NightMode.py
+++ plugins/FileBot/NightMode.py
@@ -4,11 +4,9 @@ from pyrogram import Client, filters
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ChatPermissions
 from info import CHAT_ID, TIMEZONE
-
 import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
+logging.basicConfig(level=logging.INFO)
 
 async def group_close():
     try:
