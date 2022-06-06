228d7e9fef83dad1fea6ecbf8fd72913732bdcda
God-of-Thunderr <99614535+God-of-Thunderr@users.noreply.github.com>
2022-06-06 12:33:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,17 +22,8 @@ STICKER = "CAACAgUAAxkBAAEE7AJinaTQIlVcnwmqK53UuzHZjDfq2gACqgQAAoKm8FQsM7CPCRnY_
 @Client.on_message(filters.command("start") & filters.incoming & ~filters.edited)
 async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
-        buttons = [
-            [
-                InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
-            ],
-            [
-                InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            ]
-            ]
-        reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
-        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
+                await message.reply_text"kya huaa be baar baar start kyun kar rha hai"
+                await asyncio.sleep(2) # 😢 https://github.com/God-of-Thunderr/DFF-File-Bot/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
